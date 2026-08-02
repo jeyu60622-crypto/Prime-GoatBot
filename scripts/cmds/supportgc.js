6cd61ceaeca177f8d3739befc396d079809958c4
jeyu60622-crypto <jeyu60622@gmail.com>
2026-08-02 08:04:09 +0200

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ api, event, threadsData, getLang, message }) {
-    const supportGroupThreadID = "27455554110724563"; // Replace with your support group thread ID
+    const supportGroupThreadID = "1496599381339519"; // Replace with your support group thread ID
     const botID = api.getCurrentUserID();
 
     try {
